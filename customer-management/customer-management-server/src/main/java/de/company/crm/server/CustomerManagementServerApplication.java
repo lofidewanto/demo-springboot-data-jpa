cb8c71c6dbe10ff8211d86a877915ebbb5c5d810
lofidewanto <lofidewanto@gmail.com>
2018-07-10 20:05:45 +0200

    initial

---
+++ customer-management/customer-management-server/src/main/java/de/company/crm/server/CustomerManagementServerApplication.java
@@ -0,0 +1,12 @@
+package de.company.crm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementServerApplication.class, args);
+	}
+}
